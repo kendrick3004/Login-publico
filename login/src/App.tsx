4bf525c68cfcb6f3addb93d7251060476a90a2c4
kendrick3004 <101604411+kendrick3004@users.noreply.github.com>
2022-10-16 11:06:00 -0300

    Add files via upload

---
+++ login/src/App.tsx
@@ -0,0 +1,7 @@
+import './styles/global.css';
+import type { FC } from "react";
+import {SignIn} from "./pages/SignIn";
+
+export const App: FC = () => {
+  return (
+	 <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100" </div>
